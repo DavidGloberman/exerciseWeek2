8fc60fb6b4f100b3fec4b7c43c445364c1dd2d55
02 <02@DESKTOP-A5DJ6R6>
2024-09-10 11:08:13 +0300

    arranged the functions

--- exersice3DOM/9/main.js
+++ exersice3DOM/9/main.js
@@ -3,7 +3,6 @@ let todos = [];
 function generateId() {
   return Math.random().toString(16).slice(2) + Date.now().toString(16);
 }
-
 function getLocalStorage() {
   return JSON.parse(localStorage.getItem("todos"));
 }
@@ -11,6 +10,98 @@ function setLocalStorage() {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+function clearTable() {
+  let table = document.getElementsByTagName("table")[0];
+  let tableHoder = document.getElementById("heder");
+  table.innerHTML = "";
+  table.appendChild(tableHoder);
+}
+function ChengeStatusTodo(todo) {
+  if (todo.status === "active") {
+    todo.status = "done";
+  } else if (todo.status === "done") {
+    todo.status = "active";
+  }
+}
+function editTodo(todo) {
+  let input = document.getElementsByTagName("input")[0];
+  if (input.value.trim() === "") return;
+  todo.todo = input.value.trim();
+  input.value = "";
+}
+function deleteTodo(idx) {
+  todos.splice(idx, 1);
+}
+function setActionsEventListeners(actionButtons, todo, idx) {
+  actionButtons.newButtonStatus.addEventListener("click", () => {
+    ChengeStatusTodo(todo);
+    setLocalStorage();
+    refreshTable();
+  });
+
+  actionButtons.newButtonEdit.addEventListener("click", () => {
+    editTodo(todo);
+    setLocalStorage();
+    refreshTable();
+  });
+
+  actionButtons.newButtonDelete.addEventListener("click", () => {
+    deleteTodo(idx);
+    setLocalStorage();
+    refreshTable();
+  });
+}
+
+function createActions(todo, idx) {
+  let newActions = document.createElement("td");
+
+  let newButtonStatus = document.createElement("button");
+  let newButtonEdit = document.createElement("button");
+  let newButtonDelete = document.createElement("button");
+
+  newButtonStatus.textContent = "Chenge status";
+  newButtonEdit.textContent = "Edit";
+  newButtonDelete.textContent = "Delete";
+
+  setActionsEventListeners(
+    { newButtonStatus, newButtonEdit, newButtonDelete },
+    todo,
+    idx
+  );
+
+  newActions.append(newButtonStatus, newButtonEdit, newButtonDelete);
+  return newActions;
+}
+
+function createTodoElements(todo, idx) {
+  let newRow = document.createElement("tr");
+
+  let newId = document.createElement("td");
+  let newTodo = document.createElement("td");
+  let newStatus = document.createElement("td");
+
+  newId.innerText = todo.id.substring(0, 3) + "...";
+  newTodo.innerText = todo.todo;
+  newStatus.innerText = todo.status;
+
+  if (todo.status === "active") {
+    newTodo.style.textDecoration = "none";
+  } else if (todo.status === "done") {
+    newTodo.style.textDecoration = "line-through";
+  }
+
+  let newActions = createActions(todo, idx);
+
+  newRow.append(newId, newTodo, newStatus, newActions);
+  return newRow;
+}
+function buildTable() {
+  let table = document.getElementsByTagName("table")[0];
+  todos.forEach((todo, index) => {
+    let newRow = createTodoElements(todo, index);
+    table.append(newRow);
+  });
+}
 function addTodo() {
   let input = document.getElementsByTagName("input")[0];
   if (input.value.trim() === "") return;
@@ -25,43 +116,27 @@ function addTodo() {
   input.value = "";
 
   setLocalStorage();
-  refreshTable(todos);
+  refreshTable();
+}
+function refreshTable() {
+  clearTable();
+  buildTable();
 }
 
 function sortTableAsc() {
-  todos.sort(function (a, b) {
-    let x = a.todo.toLowerCase();
-    let y = b.todo.toLowerCase();
-    if (x < y) {
-      return -1;
-    }
-    if (x > y) {
-      return 1;
-    }
-    return 0;
-  });
-  refreshTable(todos);
+  todos.sort((a, b) => a.todo.localeCompare(b.todo));
+  refreshTable();
 }
 function sortTableDesc() {
-  todos.sort(function (a, b) {
-    let x = a.todo.toLowerCase();
-    let y = b.todo.toLowerCase();
-    if (x < y) {
-      return 1;
-    }
-    if (x > y) {
-      return -1;
-    }
-    return 0;
-  });
-  refreshTable(todos);
+  todos.sort((a, b) => b.todo.localeCompare(a.todo));
+  refreshTable();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   todos = getLocalStorage() || [];
-  refreshTable(todos);
-  let button = document.getElementsByTagName("button")[0];
+  refreshTable();
 
+  let button = document.getElementsByTagName("button")[0];
   button.addEventListener("click", addTodo);
 
   const ascending = document.getElementById("ascending");
@@ -70,69 +145,3 @@ document.addEventListener("DOMContentLoaded", () => {
   ascending.addEventListener("click", sortTableAsc);
   descending.addEventListener("click", sortTableDesc);
 });
-
-function refreshTable(todos) {
-  let table = document.getElementsByTagName("table")[0];
-  let tableHoder = document.getElementById("heder");
-  table.innerHTML = "";
-  table.appendChild(tableHoder);
-
-  todos.forEach((todo, index) => {
-    let newRow = document.createElement("tr");
-    let newId = document.createElement("td");
-    let newTodo = document.createElement("td");
-    let newStatus = document.createElement("td");
-    let newActions = document.createElement("td");
-
-    let newBtnstatus = document.createElement("button");
-    newBtnstatus.textContent = "Chenge status";
-
-    let newBtnEdit = document.createElement("button");
-    newBtnEdit.textContent = "Edit";
-
-    let newBtnDelete = document.createElement("button");
-    newBtnDelete.textContent = "Delete";
-
-    newBtnstatus.addEventListener("click", () => {
-      if (todo.status === "active") {
-        todo.status = "done";
-      } else if (todo.status === "done") {
-        todo.status = "active";
-      }
-
-      setLocalStorage();
-      refreshTable(todos);
-    });
-
-    newBtnEdit.addEventListener("click", () => {
-      let input = document.getElementsByTagName("input")[0];
-      if (input.value.trim() === "") return;
-      todo.todo = input.value.trim();
-      input.value = "";
-
-      setLocalStorage();
-      refreshTable(todos);
-    });
-
-    newBtnDelete.addEventListener("click", () => {
-      todos.splice(index, 1);
-
-      setLocalStorage();
-      refreshTable(todos);
-    });
-
-    newId.innerText = todo.id.substring(0, 3) + "...";
-    newTodo.innerText = todo.todo;
-    newStatus.innerText = todo.status;
-
-    if (todo.status === "active") {
-      newTodo.style.textDecoration = "none";
-    } else if (todo.status === "done") {
-      newTodo.style.textDecoration = "line-through";
-    }
-
-    table.appendChild(newRow);
-    newRow.append(newId, newTodo, newStatus, newActions);
-    newActions.append(newBtnstatus, newBtnEdit, newBtnDelete);
-  });
-}
